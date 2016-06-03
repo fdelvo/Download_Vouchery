1143d44b4d11d334710c2c766865be755e0aeb4e
fdelv <fdelv@DESKTOP-PLHS5F3>
2016-06-03 02:27:36 +0200

    hotfix

--- Download_Vouchery/Download_Vouchery/Scripts/Angular/Controllers/VoucherController.js
+++ Download_Vouchery/Download_Vouchery/Scripts/Angular/Controllers/VoucherController.js
@@ -5,8 +5,8 @@
 
     function VoucherController($scope, $rootScope, $filter, $route, $http, FileFactory, UploadFactory, VoucherFactory) {
         $scope.Download = function () {
-            $http.get('/blobs/' + $scope.download.VoucherCode + '/download').then(function (response) {
-                window.open('/blobs/' + $scope.download.VoucherCode + '/download', '_blank', '');
+            $http.get('/api/blobs/' + $scope.download.VoucherCode + '/download').then(function (response) {
+                window.open('/api/blobs/' + $scope.download.VoucherCode + '/download', '_blank', '');
             },
             function (response) {
                 $rootScope.status = response.statusText;
